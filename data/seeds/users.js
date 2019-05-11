70a6fc860df5170f70c728a196bc372c25b16fbe
RodBromberg <rod.bromberg@yahoo.com>
2019-05-11 13:39:57 -0400

    mvp

--- data/seeds/users.js
+++ data/seeds/users.js
@@ -1,16 +1,21 @@
 exports.seed = function(knex) {
-  return knex('users')
-    .truncate()
-    .then(function() {
-      return knex('users').insert([
-        {
-          name: 'Samwise Gamgee',
-          bio: 'Gardener and poet. Married to Rose Cotton',
-        },
-        {
-          name: 'Frodo Baggins',
-          bio: 'The ring bearer',
-        },
-      ]);
-    });
+    return knex('users')
+        .truncate()
+        .then(function() {
+            return knex('users').insert([{
+                    name: 'Samwise Gamgee',
+                    bio: 'Gardener and poet. Married to Rose Cotton',
+                },
+                {
+                    name: 'Frodo Baggins',
+                    bio: 'The ring bearer',
+                },
+
+                {
+                    name: 'Turd Burg',
+                    bio: 'A Turd',
+                }
+
+            ]);
+        });
 };
